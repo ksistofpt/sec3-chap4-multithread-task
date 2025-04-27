e57b495eb6802b4aa8e0844e76b49d116b06ee5f
ksistofpt <162743912+ksistofpt@users.noreply.github.com>
2025-04-27 09:52:28 +0000

    completed Challenge Semaphore

--- src/main/java/com/codefinity/Resource.java
+++ src/main/java/com/codefinity/Resource.java
@@ -1,7 +1,21 @@
 package com.codefinity;
 
 public class Resource {
+
+    // Метод use() імітує використання ресурсу
     public void use() {
-       //TODO
+       // Виводимо повідомлення про те, що потік використовує ресурс
+       System.out.println(Thread.currentThread().getName() + " is using the resource.");
+       
+       try {
+            // Імітуємо затримку (робота з ресурсом), щоб інші потоки могли спробувати використати ресурс
+            Thread.sleep(1000); // Потік чекає 1 секунду (1000 мс)
+       } catch (InterruptedException e) {
+            // Якщо потік перерваний, відновлюємо статус переривання
+            Thread.currentThread().interrupt();
+       }
+
+       // Виводимо повідомлення про те, що потік завершив використання ресурсу
+       System.out.println(Thread.currentThread().getName() + " has finished using the resource.");
     }
 }
--- src/main/java/com/codefinity/Worker.java
+++ src/main/java/com/codefinity/Worker.java
@@ -3,9 +3,10 @@ package com.codefinity;
 import java.util.concurrent.Semaphore;
 
 public class Worker implements Runnable {
-    private final Resource resource;
-    private final Semaphore semaphore;
+    private final Resource resource;  // Ресурс, до якого потік буде звертатися
+    private final Semaphore semaphore; // Семафор для контролю доступу до ресурсу
 
+    // Конструктор, який ініціалізує ресурс і семафор
     public Worker(Resource resource, Semaphore semaphore) {
         this.resource = resource;
         this.semaphore = semaphore;
@@ -13,6 +14,18 @@ public class Worker implements Runnable {
 
     @Override
     public void run() {
-        //TODO
+        try {
+            // Потік намагається отримати дозвіл на доступ до ресурсу
+            semaphore.acquire(); // Очікує на дозвіл від семафора (блокує потік, поки не з'явиться дозвіл)
+
+            // Якщо дозвіл отримано, потік використовує ресурс
+            resource.use(); // Викликає метод use() для роботи з ресурсом
+        } catch (InterruptedException e) {
+            // Якщо потік був перерваний, відновлюємо статус переривання
+            Thread.currentThread().interrupt();
+        } finally {
+            // Після використання ресурсу потік звільняє дозвіл на використання ресурсу
+            semaphore.release(); // Звільняє дозвіл, дозволяючи іншому потоку доступити ресурс
+        }
     }
 }
